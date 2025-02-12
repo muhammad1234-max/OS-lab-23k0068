ff6712ada19208b8780837567e4e91e96b063889
Muhammad Abbas <75746019+muhammad1234-max@users.noreply.github.com>
2025-02-12 15:02:48 +0500

    Update task2.c

--- lab4/task2.c
+++ lab4/task2.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <signal.h>
-
-void alarm_handler(int signum) {
-    printf("Alarm signal received!\n");
-}
+#include <sys/types.h>
 
 int main() {
-    printf("Current Process ID: %d\n", getpid());
-    printf("Parent Process ID: %d\n", getppid());
+    pid_t pid = fork();
 
-    signal(SIGALRM, alarm_handler);  // Set up an alarm signal handler
-    alarm(5);  // Set an alarm for 5 seconds
-    printf("Sleeping for 3 seconds...\n");
-    sleep(3);  // Sleep for 3 seconds
-    printf("Woke up! Waiting for alarm...\n");
-    pause();  // Wait for the alarm signal
+    if (pid < 0) {
+        perror("Fork failed");
+        exit(1);
+    } else if (pid == 0) {  // Child process
+        for (int i = 0; i < 100; i++) {
+            printf("I am a child process\n");
+        }
+    } else {  // Parent process
+        for (int i = 0; i < 100; i++) {
+            printf("I am a parent process\n");
+        }
+    }
 
     return 0;
 }
