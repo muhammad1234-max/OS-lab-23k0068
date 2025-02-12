06a003a0cc8a55a31f3832b52bafcc993ba927a6
Muhammad Abbas <75746019+muhammad1234-max@users.noreply.github.com>
2025-02-12 15:03:06 +0500

    Update task3.c

--- lab4/task3.c
+++ lab4/task3.c
@@ -1,30 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
 #include <unistd.h>
+#include <sys/types.h>
 
 int main() {
-    int fd;
-    char buffer[100];
-
-    // Create and write to a file
-    fd = open("testfile.txt", O_CREAT | O_WRONLY, 0644);
-    if (fd < 0) {
-        perror("File creation failed");
-        exit(1);
-    }
-    write(fd, "Hello, system calls!\n", 21);
-    close(fd);
-
-    // Open and read the file
-    fd = open("testfile.txt", O_RDONLY);
-    if (fd < 0) {
-        perror("File open failed");
-        exit(1);
-    }
-    read(fd, buffer, sizeof(buffer));
-    printf("File content: %s\n", buffer);
-    close(fd);
+    printf("Current Process ID (PID): %d\n", getpid());
+    printf("Parent Process ID (PPID): %d\n", getppid());
+    printf("User ID (UID): %d\n", getuid());
 
     return 0;
 }
